63b9b09461985dfc1bce7ae8027c87d4e63aada8
Lukas Geiger <lukas.geiger94@gmail.com>
2018-02-28 17:13:09 +0100

    [tf] Check equivariance for z2/p4/pm layers

--- groupy/gconv/gconv_tensorflow/test_layers.py
+++ groupy/gconv/gconv_tensorflow/test_layers.py
@@ -1,62 +1,56 @@
+import pytest
 import numpy as np
 import tensorflow as tf
 
-from groupy.gconv.gconv_tensorflow.layers import P4ConvZ2, P4ConvP4, P4MConvZ2, P4MConvP4M
-from groupy.gfunc.z2func_array import Z2FuncArray
-from groupy.gfunc.p4func_array import P4FuncArray
-from groupy.gfunc.p4mfunc_array import P4MFuncArray
-import groupy.garray.C4_array as C4a
-import groupy.garray.D4_array as D4a
+from groupy.gconv.gconv_tensorflow.layers import P4ConvZ2, P4MConvZ2, P4ConvP4, P4MConvP4M
+from groupy.gfunc import Z2FuncArray, P4FuncArray, P4MFuncArray
+import groupy.garray.C4_array as c4a
+import groupy.garray.D4_array as d4a
 
 
-def test_c4_z2_conv_equivariance():
-    im = np.random.randn(2, 5, 5, 1)
-    x, y = make_graph(P4ConvZ2, 1)
-    check_equivariance(im, x, y, Z2FuncArray, P4FuncArray, C4a)
+@pytest.mark.parametrize('layer,input_array,output_array,point_group,ndim', [
+    (P4ConvZ2, Z2FuncArray, P4FuncArray, c4a, 1),
+    (P4MConvZ2, Z2FuncArray, P4MFuncArray, d4a, 1),
+    (P4ConvP4, P4FuncArray, P4FuncArray, c4a, 4),
+    (P4MConvP4M, P4MFuncArray, P4MFuncArray, d4a, 8)
+])
+@pytest.mark.parametrize('filters', [1, 3])
+@pytest.mark.parametrize('kernel_size', [3, 5])
+@pytest.mark.parametrize('padding', ['same', 'valid'])
+def test_equivariance(layer, input_array, output_array, point_group, ndim, filters, kernel_size, padding):
+    im = np.random.randn(1, 15, 15, ndim).astype('float32')
+    check_equivariance(
+        im=im,
+        layers=[layer(filters, kernel_size, padding=padding, use_bias=False)],
+        input_array=input_array,
+        output_array=output_array,
+        point_group=point_group,
+        filters=filters
+    )
 
 
-def test_c4_c4_conv_equivariance():
-    im = np.random.randn(2, 5, 5, 4)
-    x, y = make_graph(P4ConvP4, 4)
-    check_equivariance(im, x, y, P4FuncArray, P4FuncArray, C4a)
-
-
-def test_d4_z2_conv_equivariance():
-    im = np.random.randn(2, 5, 5, 1)
-    x, y = make_graph(P4MConvZ2, 1)
-    check_equivariance(im, x, y, Z2FuncArray, P4MFuncArray, D4a)
-
-
-def test_d4_d4_conv_equivariance():
-    im = np.random.randn(2, 5, 5, 8)
-    x, y = make_graph(P4MConvP4M, 8)
-    check_equivariance(im, x, y, P4MFuncArray, P4MFuncArray, D4a)
-
-
-def make_graph(layer, nti):
-    x = tf.placeholder(tf.float32, [None, 5, 5, 1 * nti])
-    y = layer(filters=1, kernel_size=3, use_bias=False, padding='same')(x)
-    return x, y
-
-
-def check_equivariance(im, input, output, input_array, output_array, point_group):
-
+def check_equivariance(im, layers, input_array, output_array, point_group, filters):
     # Transform the image
-    f = input_array(im.transpose((0, 3, 1, 2)))
+    f = input_array(im.transpose((0, 3, 1, 2)))  # convert to NCHW
     g = point_group.rand()
     gf = g * f
-    im1 = gf.v.transpose((0, 2, 3, 1))
+    im1 = gf.v.transpose((0, 2, 3, 1))  # convert back to NHWC
 
-    # Compute
+    x = tf.placeholder(tf.float32, im.shape)
+    output = x
+    for layer in layers:
+        output = layer(output)
+
     init = tf.global_variables_initializer()
     with tf.Session() as sess:
         sess.run(init)
-        yx = sess.run(output, feed_dict={input: im})
-        yrx = sess.run(output, feed_dict={input: im1})
+        fmap = sess.run(output, feed_dict={x: im})
+        fmap1 = sess.run(output, feed_dict={x: im1})
 
-    # Transform the computed feature maps
-    fmap1_garray = output_array(yrx.transpose((0, 3, 1, 2)))
-    r_fmap1_data = (g.inv() * fmap1_garray).v.transpose((0, 2, 3, 1))
+    n, h, w, fmap_len = fmap.shape
+    # convert to NCHW and expand feature map
+    fmap1_garray = output_array(fmap1.transpose((0, 3, 1, 2)).reshape((n, filters, fmap_len // filters, h, w)))
+    # flatten feature map andconvert back to NHWC
+    r_fmap1_data = (g.inv() * fmap1_garray).v.reshape((n, fmap_len, h, w)).transpose((0, 2, 3, 1))
 
-    print(np.abs(yx - r_fmap1_data).sum())
-    assert np.allclose(yx, r_fmap1_data, rtol=1e-5, atol=1e-3)
+    assert np.allclose(fmap, r_fmap1_data, rtol=1e-5, atol=1e-3)
